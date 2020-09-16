6af1009626294d808c078b43f152925eb151e8b0
EmmanuelD69 <emmanueldevfr@gmail.com>
2020-09-16 16:59:25 +0200

    Javascript part1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
+/* declaration des variables */
+const auth = '563492ad6f917000010000014c3f35f70fb046efb578c7d04441b1d2';
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-input');
+const submitButton = document.querySelector('.submit-btn');
+let searchValue;
 
+async function curatedPhotos() {
+	const dataFetch = await fetch(
+		'https://api.pexels.com/v1/curated?per_page=15',
+		{
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				Authorization: auth,
+			},
+		}
+	);
+	const data = await dataFetch.json();
+
+	/* intégration des photos dans la page web */
+	data.photos.forEach((photo) => {
+		/* création d'une div pour chaque photo*/
+		const galleryImg = document.createElement('div');
+		/* ajout d'une classe pour pouvoir paramétrer le Css des div */
+		galleryImg.classList.add('gallery-img');
+		/* ajout de la source de la photo et son auteur  */
+		galleryImg.innerHTML = `<img src="${photo.src.large}"></img> 
+        <p>${photo.photographer}</p>`;
+		/* ajout de la div dans l'élément parent gallery */
+		gallery.appendChild(galleryImg);
+	});
+}
+
+curatedPhotos();
